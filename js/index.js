373c2d3bc8affb0bc4cf51a595517bd3ce156d53
Jay <jay.bowen@uky.edu>
2020-06-21 18:41:57 -0500

    Add normalized data

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 // initialize our map
 var map = L.map('map');
 
-//add esri topo basemap
+// Add esri topo basemap
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
   maxZoom: 11,
   minZoom: 7,
   attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 }).addTo(map);
 
-//add a scale bar
+// Add a scale bar
 L.control.scale({
   position: 'bottomleft'
 }).addTo(map);
 
-//load the data asynchronously
+// Load the data asynchronously
 d3.queue()
   .defer(d3.json, 'https://opendata.arcgis.com/datasets/6a84756c2e444a87828bb7ce699fdac6_0.geojson') //the Iowa COVID-19 geojson file
   .await(drawMap); //load the layers after the map loads
 
-//provide instructions for drawing the map
+// Provide instructions for drawing the map
 function drawMap(err, corona) {
 
-  //get the total case count for Iowa
+  // Get the total case count for Iowa
   var caseTotal = ss.sum(corona.features.map(function(feature) {
     return feature.properties['Confirmed'];
   }));
@@ -41,12 +41,10 @@ function drawMap(err, corona) {
       'rgba(0,0,0,0.0)';
   };
 
-  //get the total death count for Iowa
+  // Get the total death count for Iowa
   var deathTotal = ss.sum(corona.features.map(function(feature) {
     return feature.properties['Deaths'];
   }));
-
-  console.log(deathTotal);
 
   // Group totals of deaths into 5 ckmeans categories
   var deathStops = ss.ckmeans(corona.features.map(function(feature) {
@@ -62,6 +60,52 @@ function drawMap(err, corona) {
       d >= ss.min(deathStops[0]) ? '#eff3ff' :
       'rgba(0,0,0,0.0)';
   };
+
+  // *** NORMALIZING THE DATA *** //
+
+  // Get an array of case counts per 1,000 people per county, rounded to two decimal places
+  var normCaseArrayPrelim = corona.features.map(function(feature) {
+    return (((feature.properties.Confirmed/feature.properties.pop_est_2018)*1000).toFixed(2));
+  });
+
+  // Remove the last entry in the array as this refers to cases pending investigation, not cases for a particular county
+  var normCaseArray = normCaseArrayPrelim.slice(0,-1);
+
+  // Group the case count per 1,000 people per county into 5 ckmeans categories
+  var normCaseStops = ss.ckmeans(normCaseArray, 5);
+
+  // Add a function to style the counties by cases/1,000 (ck means)
+  function getNormCaseColor(d) {
+    return d >= ss.min(normCaseStops[4]) ? '#08519c' :
+      d >= ss.min(normCaseStops[3]) ? '#3182bd' :
+      d >= ss.min(normCaseStops[2]) ? '#6baed6' :
+      d >= ss.min(normCaseStops[1]) ? '#bdd7e7' :
+      d >= ss.min(normCaseStops[0]) ? '#eff3ff' :
+      'rgba(0,0,0,0.0)';
+  };
+
+  // Get an array of death counts per 1,000 people per county, rounded to two decimal places
+  var normDeathArrayPrelim = corona.features.map(function(feature) {
+    return (((feature.properties.Deaths/feature.properties.pop_est_2018)*1000).toFixed(2));
+  });
+
+  // Remove the last entry in the array as this refers to cases pending investigation, not cases for a particular county
+  var normDeathArray = normDeathArrayPrelim.slice(0,-1);
+
+  // Group the death count per 1,000 people per county into 5 ckmeans categories
+  var normDeathStops = ss.ckmeans(normDeathArray, 5);
+
+  // Add a function to style the counties by deaths/1,000 (ck means)
+  function getNormDeathColor(d) {
+    return d >= ss.min(normDeathStops[4]) ? '#08519c' :
+      d >= ss.min(normDeathStops[3]) ? '#3182bd' :
+      d >= ss.min(normDeathStops[2]) ? '#6baed6' :
+      d >= ss.min(normDeathStops[1]) ? '#bdd7e7' :
+      d >= ss.min(normDeathStops[0]) ? '#eff3ff' :
+      'rgba(0,0,0,0.0)';
+  };
+
+  // *** END NORMALIZING THE DATA *** //
 
   var cases = L.geoJson(corona, { //define layer with a variable
 
@@ -80,8 +124,6 @@ function drawMap(err, corona) {
 
     //restyle on mouseover, reset style on mouseout
     onEachFeature: function(feature, layer) {
-
-      console.log(feature.properties);
 
       // bind a popup window
       layer.bindPopup('<h4><b>' + feature.properties.Name + ' County' +
@@ -157,12 +199,122 @@ function drawMap(err, corona) {
       });
     }
 
-  }); // add COVID-19 Deaths layer and style
+  }); // End COVID-19 Deaths layer and style
+
+  var normCases = L.geoJson(corona, { //define layer with a variable
+
+    style: function(feature) {
+
+      return {
+        stroke: 1,
+        color: "grey",
+        weight: 1,
+        // Use parseFloat because one result is not a number, due to the inclusion of cases pending investigation not assigned to any county
+        fillColor: getNormCaseColor(parseFloat(((feature.properties.Confirmed/feature.properties.pop_est_2018)*1000).toFixed(2))),
+        fillOpacity: 0.5
+      };
+    },
+
+    //restyle on mouseover, reset style on mouseout
+    onEachFeature: function(feature, layer) {
+
+      var props = feature.properties;
+
+      // bind a popup window
+      layer.bindPopup('<h4><b>' + props.Name + ' County' +
+        '<br>' + 'Population: ' + props.pop_est_2018 + '</b>' +
+        '<br><br>Cases per 1,000: ' + (((props.Confirmed/props.pop_est_2018)*1000).toFixed(2)) +
+        '<br>Last Updated: ' + props.last_updated.substring(0, props.last_updated.length - 8) +
+        '</h4>', {
+          maxHeight: 300,
+          minWidth: 150,
+          maxWidth: 200,
+        });
+
+      // change layer style on mouseover
+      layer.on("mouseover", function(e) {
+        layer.setStyle({
+          weight: 2,
+          color: "blue",
+          opacity: 0.75,
+          fillOpacity: 0.75,
+        }).bringToFront();
+      });
+      // reset style on mouseout
+      layer.on("mouseout", function(e) {
+        layer.setStyle({
+          stroke: 1,
+          color: "grey",
+          weight: 1,
+          // Use parseFloat because one result is not a number, due to the inclusion of cases pending investigation not assigned to any county
+          fillColor: getNormCaseColor(parseFloat(((feature.properties.Confirmed/feature.properties.pop_est_2018)*1000).toFixed(2))),
+          fillOpacity: 0.5
+        });
+      });
+    }
+
+  }); // End COVID-19 Cases per 1,000 layer and style
+
+  var normDeaths = L.geoJson(corona, { //define layer with a variable
+
+    style: function(feature) {
+
+      var props = feature.properties;
+
+      return {
+        stroke: 1,
+        color: "grey",
+        weight: 1,
+        fillColor: getNormDeathColor(((props.Deaths/props.pop_est_2018)*1000).toFixed(2)),
+        fillOpacity: 0.5
+      };
+    },
+
+    //restyle on mouseover, reset style on mouseout
+    onEachFeature: function(feature, layer) {
+
+      var props = feature.properties;
+
+      // bind a popup window
+      layer.bindPopup('<h4><b>' + props.Name + ' County' +
+        '<br>' + 'Population: ' + props.pop_est_2018 + '</b>' +
+        '<br><br>Deaths per 1,000: ' + (((props.Deaths/props.pop_est_2018)*1000).toFixed(2)) +
+        '<br>Last Updated: ' + props.last_updated.substring(0, props.last_updated.length - 8) +
+        '</h4>', {
+          maxHeight: 300,
+          minWidth: 150,
+          maxWidth: 200,
+        });
+
+      // change layer style on mouseover
+      layer.on("mouseover", function(e) {
+        layer.setStyle({
+          weight: 2,
+          color: "blue",
+          opacity: 0.75,
+          fillOpacity: 0.75,
+        }).bringToFront();
+      });
+      // reset style on mouseout
+      layer.on("mouseout", function(e) {
+        layer.setStyle({
+          stroke: 1,
+          color: "grey",
+          weight: 1,
+          fillColor: getNormDeathColor(((props.Deaths/props.pop_est_2018)*1000).toFixed(2)),
+          fillOpacity: 0.5
+        });
+      });
+    }
+
+  }); // End COVID-19 Deaths per 1,000 layer and style
 
   //define layers
   var baselayers = {
     "Confirmed Cases": cases,
     "Confirmed Deaths": deaths,
+    "Cases/1,000 Pop.": normCases,
+    "Deaths/1,000 Pop.": normDeaths,
   };
 
   //send the layers to the layer control
@@ -184,12 +336,20 @@ function drawMap(err, corona) {
     }]
   }).addTo(map);
 
-  // create legends for both layers
+  // create legends for layers
   var caseLegend = L.control({
     position: 'bottomright'
   });
 
   var deathLegend = L.control({
+    position: 'bottomright'
+  });
+
+  var normCaseLegend = L.control({
+    position: 'bottomright'
+  });
+
+  var normDeathLegend = L.control({
     position: 'bottomright'
   });
 
@@ -227,6 +387,41 @@ function drawMap(err, corona) {
     return div;
   };
 
+  // add content to the legend
+  normCaseLegend.onAdd = function(map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [ss.min(normCaseStops[0]), ss.min(normCaseStops[1]), ss.min(normCaseStops[2]), ss.min(normCaseStops[3]), ss.min(normCaseStops[4])],
+      labels = ["<h6 style='font-size:14px; font-weight:bold'>Cases Per 1,000 People</h6>"];
+
+    // loop through our intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        // Use parseFloat because one result is not a number, due to the inclusion of cases pending investigation not assigned to any county
+        labels.push('<i style="background:' + getNormCaseColor(parseFloat(grades[i])) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + (grades[i + 1] - 0.01).toFixed(2) : '+'));
+    }
+    div.innerHTML = labels.join('<br>');
+    return div;
+  };
+
+  // add content to the legend
+  normDeathLegend.onAdd = function(map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [ss.min(normDeathStops[0]), ss.min(normDeathStops[1]), ss.min(normDeathStops[2]), ss.min(normDeathStops[3]), ss.min(normDeathStops[4])],
+      labels = ["<h6 style='font-size:14px; font-weight:bold'>Deaths Per 1,000 People</h6>"];
+
+    // loop through our intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        labels.push('<i style="background:' + getNormDeathColor(grades[i]) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + (grades[i + 1] - 0.01).toFixed(2) : '+'));
+    }
+    div.innerHTML = labels.join('<br>');
+    return div;
+  };
+
   // add this legend first, because this layer is on by default
   caseLegend.addTo(map);
 
@@ -235,10 +430,24 @@ function drawMap(err, corona) {
     // Switch to the Confirmed Cases legend...
     if (eventLayer.name === 'Confirmed Cases') {
       this.removeControl(deathLegend);
+      this.removeControl(normCaseLegend);
+      this.removeControl(normDeathLegend);
       caseLegend.addTo(this);
-    } else { // Or switch to the Confirmed Deaths legend...
+    } if (eventLayer.name === 'Confirmed Deaths') { // Or switch to the Confirmed Deaths legend...
       this.removeControl(caseLegend);
+      this.removeControl(normCaseLegend);
+      this.removeControl(normDeathLegend);
       deathLegend.addTo(this);
+    } if (eventLayer.name === 'Cases/1,000 Pop.') { // Or switch to the Cases/1,000 Pop. legend...
+      this.removeControl(caseLegend);
+      this.removeControl(deathLegend);
+      this.removeControl(normDeathLegend);
+      normCaseLegend.addTo(this);
+    } if (eventLayer.name === 'Deaths/1,000 Pop.') { // Or switch to the Deaths/1,000 Pop. legend...
+      this.removeControl(caseLegend);
+      this.removeControl(deathLegend);
+      this.removeControl(normCaseLegend);
+      normDeathLegend.addTo(this);
     }
   });
 
